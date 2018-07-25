21aa4706f407b252ee81eaedb41955e5cffc4c71
Juraj Selep <jurajselep@gmail.com>
2018-07-25 20:56:41 +0200

    Small network refactor

--- lib/rpc.d.ts
+++ lib/rpc.d.ts
@@ -1,2 +1,8 @@
 import { Observable } from 'rxjs';
+export declare const config: () => {
+    api: string;
+    header: {
+        'Content-Type': string;
+    };
+};
 export declare const rpc: (url: string, payload?: any) => Observable<any>;
--- rpc.ts
+++ rpc.ts
@@ -2,23 +2,25 @@ import { Observable } from 'rxjs';
 import { map, filter } from 'rxjs/operators';
 import { ajax } from 'rxjs/ajax';
 
-const config = {
-    api: 'https://zeronet.simplestaking.com:3000',
-    //api: 'https://node2.simplestaking.com:3000',
-    header: { 'Content-Type': 'application/json' }
+export const config = () => {
+    return {
+        api: 'https://zeronet.simplestaking.com:3000',
+        //api: 'https://node2.simplestaking.com:3000',
+        header: { 'Content-Type': 'application/json' }
+    }
 }
 
 export const rpc = (url: string, payload?: any) => {
 
     return payload !== undefined ?
-        ajax.post(config.api + url, payload, config.header).pipe(
+        ajax.post(config().api + url, payload, config().header).pipe(
             // if do not have response do not run it
             filter(event => event.response),
             // use only response
             map(event => event.response)
         )
         :
-        ajax.get(config.api + url, config.header).pipe(
+        ajax.get(config().api + url, config().header).pipe(
             // if do not have response do not run it
             filter(event => event.response),
             // use only response
