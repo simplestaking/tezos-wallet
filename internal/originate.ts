17ad0072bd4e16d6d6248c0d8e57a9055c69e7b5
Juraj Selep <jurajselep@gmail.com>
2019-11-02 23:19:01 +0100

    Added smart contract

---
+++ internal/originate.ts
@@ -0,0 +1,63 @@
+import { of } from 'rxjs'
+import { tap } from 'rxjs/operators';
+
+import { Config, initializeWallet, transaction, pendingOperation, confirmOperation, validateOriginationOperation, originateContract } from '../src'
+
+// support for node.js
+import './node'
+
+console.log('[+] tezos wallet client')
+
+// read smart contact script
+let smartContractScript = require('./contracts/catalog.json');
+
+// wallet used to create transaction with small tez amount
+const wallet: Config = {
+    secretKey: 'edsk3hEtniBGLP2wqYnc1Lix1hhV74eEenRBTfxjHTRHP59R7BapMW',
+    publicKey: 'edpkth42B7j7rvLeZWmufj28a7sEdbMBb9y16qgQmGHYGX4hPis9V4',
+    publicKeyHash: 'tz1WCojrEZWrjenejUZmG8QNsMtKPELx2TFA',
+    node: {
+        name: 'babylon',
+        display: 'Babylon',
+        url: 'https://alphanet.simplestaking.com:3000',
+        tzstats: {
+            url: 'http://babylonnet.tzstats.com/account/',
+        }
+    },
+    type: 'web'
+}
+
+const walletObservable = of([])
+
+// create observable with state  
+walletObservable.pipe(
+
+    // wait for sodium to initialize
+    initializeWallet(stateWallet => ({
+        secretKey: wallet.secretKey,
+        publicKey: wallet.publicKey,
+        publicKeyHash: wallet.publicKeyHash,
+        // set Tezos node
+        node: wallet.node,
+        // set wallet type: WEB, TREZOR_ONE, TREZOR_T
+        type: wallet.type,
+    })),
+
+    // originate new smart contract
+    originateContract(stateWallet => ({
+        amount: "100",
+        fee: "0.3",
+        script: smartContractScript,                                 
+    })),
+
+
+    // wait until operation is confirmed & moved from mempool to head
+    confirmOperation(stateWallet => ({
+        injectionOperation: stateWallet.injectionOperation,
+    })),
+
+
+).subscribe(
+        data => console.log('[+] ok'),
+        error => console.error('[-] error', error)
+    )
